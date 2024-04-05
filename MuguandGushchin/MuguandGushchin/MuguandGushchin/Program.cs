ff628d75cf73f87c793d04b6fe30f16cfac121a6
Tess_OK <88114924+SexDJ@users.noreply.github.com>
2024-04-05 14:11:28 +0300

    Add files via upload

---
+++ MuguandGushchin/MuguandGushchin/MuguandGushchin/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Employee
+{
+    public string FIO { get; set; }
+    public string OBRAZOVANIE { get; set; }
+    public string DOLZHNOST { get; set; }
+    public double OKLAD { get; set; }
+
+    public Employee(string fullName, string education, string position, double salary)
+    {
+        FIO = fullName;
+        OBRAZOVANIE = education;
+        DOLZHNOST = position;
+        OKLAD = salary;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Информация о сотруднике:");
+        Console.WriteLine("ФИО: " + FIO);
+        Console.WriteLine("Образование: " + OBRAZOVANIE);
+        Console.WriteLine("Должность: " + DOLZHNOST);
+        Console.WriteLine("Оклад: " + OKLAD);
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee employee1 = new Employee("Гущин Дмитрий Сигма", "Высшее", "Бизнесмен", 50000000.00);
+        employee1.DisplayInfo();
+
+        Employee employee2 = new Employee("Мугу Самир Рустамович", "Высшее", "Предприниматель", 13000000.00);
+        employee2.DisplayInfo();
+    }
+}
